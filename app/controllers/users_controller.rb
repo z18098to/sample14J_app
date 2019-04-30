b91c8fdc4302d798ec972c67d254af07eee7b286
z18098to <z18098to@aiit.ac.jp>
2019-04-30 11:32:34 +0000

    Ch7.4 update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,29 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     #debugger
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    #if @user.false  #演習
+      flash[:success] = "Welcome to the Sample App!"
+      # 保存の成功をここで扱う。
+      #redirect_to @user 下と同じ意味
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+  
+  
 end
