c4928b0a39197c0231c4a87d96cf798d7dc7ea6d
z18098to <z18098to@aiit.ac.jp>
2019-05-02 19:17:27 +0000

    Ch10.3.2 update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,5 +40,10 @@ test "should get about" do
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup  #演習10.2.3で追加
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,9 +13,25 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path #演習10.2.3
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+
+#演習10.2.3で追加した分（以下)
+  test "layout links when logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
