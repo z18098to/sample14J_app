85a9ecda2680e605ed6dfa13ec6aa36fcb185f3a
z18098to <z18098to@aiit.ac.jp>
2019-05-19 08:35:29 +0000

    Update 13.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     #debugger
+    
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
