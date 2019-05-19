0ed263e3066adcf3ff47c7589877b79771687391
z18098to <z18098to@aiit.ac.jp>
2019-05-19 07:56:40 +0000

    Update 13.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  
   attr_accessor :remember_token  
   #before_save { self.email = email.downcase }
   before_save { email.downcase! }  #cf 6.2.5 演習1
