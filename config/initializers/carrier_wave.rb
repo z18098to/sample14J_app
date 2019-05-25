5ba9e6707b99429decc86f9f2fc024bea2ab44a1
z18098to <z18098to@aiit.ac.jp>
2019-05-25 09:46:37 +0000

    CH13 gem modified

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Amazon S3用の設定
-      :provider              => 'fog/aws', #'AWS',
+      :provider              => 'AWS', #'fog/aws', #'AWS',
       :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
